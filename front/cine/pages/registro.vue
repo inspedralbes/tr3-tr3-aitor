d9a7e84a4fe2b7426fc1b1047e245722404632d8
Aitor <a20aitbaresc@inspedralbes.cat>
2024-04-02 13:04:15 +0200

    mejorar el css

--- front/cine/pages/registro.vue
+++ front/cine/pages/registro.vue
@@ -1,179 +1,213 @@
 <template>
-    <div class="registro">
-      <h1>Registro</h1>
-      <form @submit.prevent="submitRegistro">
-        <div class="campo">
-          <label for="email">Correo electrónico:</label>
-          <input type="email" id="email" v-model="registro.email" required>
+    <div class="page-container">
+        <div class="registro">
+            <h2 class="form-title">Registro</h2>
+            <form @submit.prevent="submitRegistro">
+                <div class="campo">
+                    <label for="email">Correo electrónico:</label>
+                    <input type="email" id="email" v-model="registro.email" required>
+                </div>
+                <div class="campo">
+                    <label for="password">Contraseña:</label>
+                    <input type="password" id="password" v-model="registro.password" required>
+                </div>
+                <div class="campo">
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" id="nombre" v-model="registro.nombre" required>
+                </div>
+                <div class="campo">
+                    <label for="apellido">Apellido:</label>
+                    <input type="text" id="apellido" v-model="registro.apellido" required>
+                </div>
+                <div class="campo">
+                    <label for="foto">Foto de perfil:</label>
+                    <button @click="mostrarModal">Seleccionar foto</button>
+                </div>
+                <button type="submit">Registrarse</button>
+            </form>
+
+            <!-- Modal de selección de imagen -->
+            <div class="modal" :class="{ 'abierto': modalAbierto }">
+                <div class="contenido">
+                    <span class="cerrar" @click="cerrarModal">&times;</span>
+                    <h2>Seleccionar foto de perfil</h2>
+                    <div class="opciones">
+                        <div class="fila-imagenes">
+                            <img src="../public/icoshin.png" alt="Foto 1" @click="seleccionarFoto(1)">
+                            <img src="../public/icodoraemon.png" alt="Foto 2" @click="seleccionarFoto(2)">
+                            <img src="../public/icowalle.png" alt="Foto 3" @click="seleccionarFoto(3)">
+                        </div>
+                        <div class="fila-imagenes">
+                            <img src="../public/icospiderman.png" alt="Foto 4" @click="seleccionarFoto(4)">
+                            <img src="../public/icoant.png" alt="Foto 5" @click="seleccionarFoto(5)">
+                            <img src="../public/icogroot.png" alt="Foto 6" @click="seleccionarFoto(6)">
+                        </div>
+                        <div class="fila-imagenes">
+                            <img src="../public/icocars.png" alt="Foto 7" @click="seleccionarFoto(7)">
+                            <img src="../public/icohomer.png" alt="Foto 8" @click="seleccionarFoto(8)">
+                            <img src="../public/icominion.png" alt="Foto 9" @click="seleccionarFoto(9)">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="campo">
-          <label for="password">Contraseña:</label>
-          <input type="password" id="password" v-model="registro.password" required>
-        </div>
-        <div class="campo">
-          <label for="nombre">Nombre:</label>
-          <input type="text" id="nombre" v-model="registro.nombre" required>
-        </div>
-        <div class="campo">
-          <label for="apellido">Apellido:</label>
-          <input type="text" id="apellido" v-model="registro.apellido" required>
-        </div>
-        <div class="campo">
-          <label for="foto">Foto de perfil:</label>
-          <button @click="mostrarModal">Seleccionar foto</button>
-        </div>
-        <button type="submit">Registrarse</button>
-      </form>
-  
-      <!-- Modal de selección de imagen -->
-      <div class="modal" :class="{ 'abierto': modalAbierto }">
-        <div class="contenido">
-          <span class="cerrar" @click="cerrarModal">&times;</span>
-          <h2>Seleccionar foto de perfil</h2>
-          <div class="opciones">
-            <img src="../public/icoshin.png" alt="Foto 1" @click="seleccionarFoto(1)">
-            <img src="../public/icodoraemon.png" alt="Foto 2" @click="seleccionarFoto(2)">
-            <img src="../public/icowalle.png" alt="Foto 3" @click="seleccionarFoto(3)">
-            <img src="../public/icospiderman.png" alt="Foto 4" @click="seleccionarFoto(4)">
-            <img src="../public/icoant.png" alt="Foto 5" @click="seleccionarFoto(5)">
-            <img src="../public/icogroot.png" alt="Foto 6" @click="seleccionarFoto(6)">
-            <img src="../public/icocars.png" alt="Foto 7" @click="seleccionarFoto(7)">
-            <img src="../public/icohomer.png" alt="Foto 8" @click="seleccionarFoto(8)">
-          </div>
-        </div>
-      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
+</template>
+
+<script>
+export default {
     data() {
-      return {
-        registro: {
-          email: '',
-          password: '',
-          nombre: '',
-          apellido: '',
-          fotoPerfil: null // Usaremos null para indicar que aún no se ha seleccionado ninguna foto
-        },
-        modalAbierto: false
-      };
+        return {
+            registro: {
+                email: '',
+                password: '',
+                nombre: '',
+                apellido: '',
+                fotoPerfil: null // Usaremos null para indicar que aún no se ha seleccionado ninguna foto
+            },
+            modalAbierto: false
+        };
     },
     methods: {
-      submitRegistro() {
-        console.log('Datos de registro:', this.registro);
-        // Aquí podrías enviar los datos de registro a tu servidor
-      },
-      mostrarModal() {
-        this.modalAbierto = true;
-      },
-      cerrarModal() {
-        this.modalAbierto = false;
-      },
-      seleccionarFoto(numero) {
-        this.registro.fotoPerfil = numero;
-        this.cerrarModal(); // Cerrar el modal después de seleccionar una foto
-      }
+        submitRegistro() {
+            console.log('Datos de registro:', this.registro);
+            // Aquí podrías enviar los datos de registro a tu servidor
+        },
+        mostrarModal() {
+            this.modalAbierto = true;
+        },
+        cerrarModal() {
+            this.modalAbierto = false;
+        },
+        seleccionarFoto(numero) {
+            this.registro.fotoPerfil = numero;
+            this.cerrarModal(); // Cerrar el modal después de seleccionar una foto
+        }
     }
-  };
-  </script>
-  <style scoped>
-  .registro {
+};
+</script>
+<style scoped>
+/* Estilos generales */
+.page-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 70vh;
+    background-color: #000;
+}
+
+.registro {
+    width: 100%;
     max-width: 400px;
-    margin: 0 auto;
     padding: 20px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    background-color: #f9f9f9;
-  }
-  
-  .campo {
+    background-color: #000;
+    color: #fff;
+}
+
+.form-title {
+    color: #fff;
+    text-align: center;
+}
+
+.campo {
     margin-bottom: 15px;
-  }
-  
-  label {
+}
+
+label {
     display: block;
     margin-bottom: 5px;
     font-weight: bold;
-  }
-  
-  input[type="text"],
-  input[type="email"],
-  input[type="password"],
-  select {
+}
+
+input[type="text"],
+input[type="email"],
+input[type="password"],
+select {
     width: 100%;
     padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 3px;
-  }
-  
-  button {
+    border-radius: 5px;
+    background-color: #333;
+    color: #fff;
+}
+
+button {
     width: 100%;
     padding: 10px;
     border: none;
     border-radius: 3px;
-    background-color: #007bff;
+    background-color: #f00;
     color: #fff;
     cursor: pointer;
-  }
-  
-  button:hover {
-    background-color: #0056b3;
-  }
-  
-  /* Estilos para el modal */
-  .modal {
+}
+
+button:hover {
+    background-color: #c00;
+}
+
+/* Estilos para el modal */
+.modal {
     position: fixed;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(0, 0, 0, 0.5); /* Fondo semitransparente */
+    background-color: rgba(0, 0, 0, 0.5);
     z-index: 9999;
     display: none;
     justify-content: center;
     align-items: center;
-  }
-  
-  .modal.abierto {
-    display: flex; /* Mostrar el modal cuando está abierto */
-  }
-  
-  .modal .contenido {
+}
+
+.modal.abierto {
+    display: flex;
+}
+
+.modal .contenido {
     background-color: #fff;
     padding: 20px;
     border-radius: 5px;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
     position: relative;
-  }
-  
-  .modal .contenido .cerrar {
+}
+
+.modal .contenido .cerrar {
     position: absolute;
     top: 10px;
     right: 10px;
     cursor: pointer;
     font-size: 20px;
     color: #888;
-  }
-  
-  .modal h2 {
+}
+
+.modal h2 {
     margin-top: 0;
-  }
-  
-  .modal .opciones img {
-    width: 100px;
+}
+
+.modal .opciones img {
+    width: 150px;
+    height: 150px;
+    margin-left: 10px;
     margin-right: 10px;
+    margin-bottom: 10px;
+    border-radius: 50%;
     cursor: pointer;
-  }
-  
-  .modal .opciones img:last-child {
+    transition: transform 0.3s ease;
+}
+
+.modal .opciones img:last-child {
     margin-right: 0;
-  }
-  
-  /* Estilo para las imágenes cuando se hace hover sobre ellas */
-  .modal .opciones img:hover {
-    border: 2px solid #007bff; /* Cambia el color del borde al color deseado */
-    border-radius: 5px; /* Agrega un borde redondeado */
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.5); /* Agrega una sombra al hacer hover */
-  }
-  </style>
+}
+
+/* Estilo para las imágenes cuando se hace hover sobre ellas */
+.modal .opciones img:hover {
+    border: 2px solid #007bff;
+    transform: scale(1.1);
+}
+
+/* Estilo para las filas de imágenes */
+.opciones .fila-imagenes {
+    display: flex;
+    justify-content: space-around;
+    margin-bottom: 10px;
+}
+</style>
