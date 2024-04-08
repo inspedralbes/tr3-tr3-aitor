401695f5dac75be78afdfbd6a34fe0824d99aaef
Aitor <a20aitbaresc@inspedralbes.cat>
2024-04-08 13:59:59 +0200

    funcion enviar email

---
+++ back/api-cine/resources/views/mail/hello.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confirmación de Compra</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #ffffff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h1, h2 {
+            color: #333333;
+        }
+
+        img {
+            max-width: 100%;
+            height: auto;
+            display: block;
+            margin: 20px auto;
+            border-radius: 8px;
+        }
+
+        p {
+            color: #666666;
+        }
+
+        .footer {
+            margin-top: 20px;
+            text-align: center;
+            color: #999999;
+            font-size: 12px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Hola, buenas,</h1>
+        <p>Aquí tienes las entradas que has comprado, Señor/a Aitor Barreiro Escobar:</p>
+        <p><strong>Sesión:</strong> 4</p>
+        <h2>Película: Fast and Furious 9</h2>
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.sensacine.com%2Fpeliculas%2Fpelicula-224392%2F&psig=AOvVaw2Xl5ysCJ6WK3-9VFh_olIV&ust=1712663925338000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNCnw7jIsoUDFQAAAAAdAAAAABAE" alt="Fast and Furious 9">
+        <p><strong>Fecha:</strong> 2021-06-01</p>
+        <p><strong>Fila:</strong> 1</p>
+        <p><strong>Columna:</strong> 2</p>
+        <p><strong>Precio:</strong> $10.00</p> <!-- Aquí se muestra el precio -->
+    </div>
+    <div class="footer">
+        <p>Este correo es generado automáticamente. Por favor, no responda a este mensaje.</p>
+    </div>
+</body>
+</html>
--- back/api-cine/routes/web.php
+++ back/api-cine/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\EnviarCorreo;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    Mail::to('aitorbares018@gmail.com')
+    ->send (new EnviarCorreo());
 });
